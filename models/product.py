82ce679afdf4ae3a2a66b29b6aebd9262e99b195
Akinpelutee <temitopeakinpelu98@gmail.com>
2025-07-23 08:14:41 +0100

    Changed the Product class to uppercase

--- models/product.py
+++ models/product.py
@@ -1,7 +1,7 @@
 from app import db
 
 
-class product(db.Model):
+class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=True)
