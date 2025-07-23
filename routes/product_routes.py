b8c49ce85df7c3ef30028e1c97d95cc9e3773e58
Akinpelutee <temitopeakinpelu98@gmail.com>
2025-07-23 10:44:43 +0100

    refactor file

--- routes/product_routes.py
+++ routes/product_routes.py
@@ -1,11 +1,14 @@
 from flask_restful import Resource
-from models.cart import Cart
+from models.product import Product
 from extensions import db
 
-class CartList(Resource):
+class ProductList(Resource):
     def post(self):
-        data = {'user_id': 1, 'product_id': 1, 'quantity': 2}
-        cart = Cart(user_id=data['user_id'], product_id=data['product_id'], quantity=data['quantity'])
-        db.session.add(cart)
+        data = {'name': 'Product 1', 'description': 'This is product 1', 'price': 10.99, 'quantity': 100}
+        product = Product(name=data['name'], description=data['description'], price=data['price'], quantity=data['quantity'])
+        db.session.add(product)
         db.session.commit()
-        return {'message': 'Product added to Cart'}
+        return {'message': 'Product created successfully'}
+
+def init_routes(api):
+    api.add_resource(ProductList, '/products')
