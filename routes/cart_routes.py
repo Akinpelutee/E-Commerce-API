11dc3356d4e9d5e572935cf36a5528f10f9d90cc
Akinpelutee <temitopeakinpelu98@gmail.com>
2025-07-23 10:54:08 +0100

    refactored the codes

--- routes/cart_routes.py
+++ routes/cart_routes.py
@@ -1,18 +1,18 @@
-from flask import request, jsonify
 from flask_restful import Resource
 from models.cart import Cart
 from extensions import db
 
 class CartList(Resource):
-    def get(self, user_id):
-        carts = Cart.query.filter_by(user_id=user_id).all()
-        return [{'id':cart.id, 'product_id':cart.product_id, 'quantity':cart.quantity} for cart in carts]
-    
-
     def post(self, user_id):
-        data = request.get_json()
+        data = {'product_id': 1, 'quantity': 2}
         cart = Cart(user_id=user_id, product_id=data['product_id'], quantity=data['quantity'])
         db.session.add(cart)
         db.session.commit()
-        return {'message':'Product added to Cart successfully'}
-    
+        return {'message': 'Product added to cart successfully'}
+
+    def get(self, user_id):
+        carts = Cart.query.filter_by(user_id=user_id).all()
+        return [{'id': cart.id, 'product_id': cart.product_id, 'quantity': cart.quantity} for cart in carts]
+
+def init_routes(api):
+    api.add_resource(CartList, '/users/<int:user_id>/cart')
--- routes/order_routes.py
+++ routes/order_routes.py
@@ -1,17 +1,18 @@
-# routes/order_routes.py
-from flask import request, jsonify
 from flask_restful import Resource
 from models.order import Order
 from extensions import db
 
 class OrderList(Resource):
-    def get(self, user_id):
-        orders = Order.query.filter_by(user_id=user_id).all()
-        return [{'id': order.id, 'order_date': order.order_date, 'total': order.total} for order in orders]
-
     def post(self, user_id):
-        data = request.get_json()
+        data = {'total': 21.98}
         order = Order(user_id=user_id, total=data['total'])
         db.session.add(order)
         db.session.commit()
         return {'message': 'Order placed successfully'}
+
+    def get(self, user_id):
+        orders = Order.query.filter_by(user_id=user_id).all()
+        return [{'id': order.id, 'order_date': order.order_date, 'total': order.total} for order in orders]
+
+def init_routes(api):
+    api.add_resource(OrderList, '/users/<int:user_id>/orders')
